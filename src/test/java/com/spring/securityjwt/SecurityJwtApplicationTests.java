b1cb2fb1cbdc16c4d9cfcf45b155a9ce6f91704a
Yoon <yoondev93@gmail.com>
2021-04-13 13:51:21 +0900

    Init

---
+++ src/test/java/com/spring/securityjwt/SecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.securityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
